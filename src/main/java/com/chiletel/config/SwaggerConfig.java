d4fc328e95e26d1373630beed1e6a25d8c66e9a9
BRAYAN ALIRIO BAQUERO CARDOZO <1015453029@bolivar.com>
2021-06-13 11:31:32 -0500

    Swagger y Auth jwt

--- src/main/java/com/chiletel/config/SwaggerConfig.java
+++ src/main/java/com/chiletel/config/SwaggerConfig.java
@@ -3,17 +3,15 @@ package com.chiletel.config;
 
 
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.google.common.collect.Lists;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,27 +19,58 @@ import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+
+
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
+//@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer{
 	
 	@Bean
 	public Docket api() {
 		
 	 return new Docket(DocumentationType.SWAGGER_2)
+			 	.apiInfo(apiinfo())
+			 	.securityContexts(Arrays.asList(securityContext()))
+			 	.securitySchemes(Arrays.asList(apikey()))
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.chiletel"))
+				//.apis(RequestHandlerSelectors.basePackage("com.chiletel"))
+				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build();
 	}
-
 	
+	private ApiKey apikey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+	private List<SecurityReference> defaultAuth(){
+		AuthorizationScope authorizationScope=new AuthorizationScope("glocal", "accessEverything");
+		AuthorizationScope[] authorizationScopes=new AuthorizationScope[1];
+		authorizationScopes[0]=authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+	}
+
+	private ApiInfo apiinfo() {
+		return new ApiInfo(
+				"Chiletel",
+				"Descripcion",
+				"2.0",
+				"Terminos y condiciones",
+				new Contact("Brayan Baquero", "www.contact.com", "brayan.baquero@segurosbolibar.com"),
+				"Licencia", 
+				"www.licencia.com", 
+				Collections.emptyList()
+				);
+	}
 	
 
    
--- src/main/java/com/chiletel/security/MainSecurity.java
+++ src/main/java/com/chiletel/security/MainSecurity.java
@@ -63,58 +63,27 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
 		return super.authenticationManager();
 	}
 	
-	 private static final List<String> AUTH_LIST = Arrays.asList(
-	            "/swagger-resources/**",
-	            "/swagger-ui.html**",
-	            "/webjars/**",
-	            "favicon.ico");
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.cors().and().csrf().disable()
 			.authorizeRequests()
-			.antMatchers( HttpMethod.GET,
-					"/swagger-resources/**",
-			        "/swagger-ui.html",
-			        "/v2/api-docs",
-			        "/webjars/**").permitAll()
-			.antMatchers("/auth/**").permitAll()
+			.antMatchers("/auth/**",
+						"/v2/api-docs/**",
+						"/swagger-ui/**",
+						"/swagger-resources/**",
+						"/configuration/**"
+			).permitAll()
 			//.antMatchers("/**").permitAll()
 			.anyRequest().authenticated()
-			.and().exceptionHandling()
-            .defaultAuthenticationEntryPointFor(swaggerAuthenticationEntryPoint(), new CustomRequestMatcher(AUTH_LIST))
-            .and()
-            .httpBasic()
 			.and()
 			.exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
-    @Bean
-    public BasicAuthenticationEntryPoint swaggerAuthenticationEntryPoint() {
-        BasicAuthenticationEntryPoint entryPoint = new BasicAuthenticationEntryPoint();
-        entryPoint.setRealmName("Swagger Realm");
-        return entryPoint;
-    }
+ 
 
-    private class CustomRequestMatcher implements RequestMatcher {
-
-        private List<AntPathRequestMatcher> matchers;
-
-        private CustomRequestMatcher(List<String> matchers) {
-            this.matchers = matchers.stream().map(AntPathRequestMatcher::new).collect(Collectors.toList());
-        }
-
-        @Override
-        public boolean matches(HttpServletRequest request) {
-            return matchers.stream().anyMatch(a -> a.matches(request));
-        }
-
-    }
-	
-
-	
-	
 }
